f1c322bf44a9d0444caa665001a11e4eb5c6ac61
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-05-19 13:01:27 +1000

    TodoList component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+
+function TodoList ({text}){
+  return (
+    <div className="todolist-container">
+      <div className="todolist-text">
+        {text}
+      </div>
+      <div className="todolist-icon">
+
+      </div>
+    </div>
+  )
+}
+export default TodoList
