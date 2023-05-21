b0a09ee715877f4d75f4a0ffee985795cfef869b
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-05-21 20:14:19 +1000

    add to Form component 'onSubmit' prop

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -8,18 +8,27 @@ function Manager() {
   const [todoList, setTodoList] = useState([]);
 
   const addTodoList = todoList => {
+   
     console.log('add Text')
     console.log(todoList)
   }
 
   return(
     <>
-      <Form />
+      <Form 
+        /* This prop send the an argument already set in Form.jsx(an object in this case)
+         to the function that is inside the bracket(in this case 'addTodoList') this
+         fuction takes an argument 'todoList' ('todoList' value argument in this case
+         is 'newTodoList' object from Form.jsx) and that argument/value will be access 
+         inside addTodoList function, after that argument is use in 'useState' */
+        
+        onSubmit={addTodoList}
+      />
       <div className='list-container'>
        {
         todoList.map((todoList,index) =>
           <TodoList 
-            text={todoList.text}
+            // text={todoList.text}
             /*props to know is the the task have been click and it will 
             change the style. this a boolean*/ 
             isDone={todoList.isDone}
